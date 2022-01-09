318c900851b8aaf03bc3c0b8946405ae3827c288
pedrovic7997 <pedrovictor6974@gmail.com>
2022-01-08 21:29:52 -0300

    Criando serviços e usando injeção de dependências

--- src/app/cursos/cursos.module.ts
+++ src/app/cursos/cursos.module.ts
@@ -5,6 +5,8 @@ import { CommonModule } from '@angular/common';
 import { CursosComponent } from './cursos.component';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
+import { CursosService } from './cursos.service';
+
 @NgModule({
   declarations: [
     CursosComponent,
@@ -15,6 +17,9 @@ import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
   ],
   exports: [
     CursosComponent
+  ],
+  providers: [
+    CursosService
   ]
 })
 export class CursosModule { }
